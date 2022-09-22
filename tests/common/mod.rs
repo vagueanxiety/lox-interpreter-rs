c71c07f50ca5ad6f960b457cd9c48b4ba641a385
Zhihang Zhang <hizhihang@gmail.com>
2022-09-21 20:24:43 -0700

    [test] add tests for blocks and scopes

---
+++ tests/common/mod.rs
@@ -0,0 +1,29 @@
+use lox_interpreter_rs::Interpreter;
+use pretty_assertions::assert_eq;
+use std::fs;
+
+pub fn run_test(test_name: &str, check_output: bool, check_error: bool) {
+    let file_name = format!("tests/{test_name}.lox");
+    let input = fs::read_to_string(format!("{file_name}")).expect("Failed to read input file");
+
+    let mut output = Vec::new();
+    let mut error_output = Vec::new();
+
+    let mut it = Interpreter::new();
+    it.run(input.to_string(), &mut output, &mut error_output)
+        .expect("Interpreter Error");
+    let output = String::from_utf8(output).expect("Not UTF-8");
+    let error_output = String::from_utf8(error_output).expect("Not UTF-8");
+
+    if check_output {
+        let expected_output =
+            fs::read_to_string(format!("{file_name}.o")).expect("Failed to read output file");
+        assert_eq!(output, expected_output);
+    }
+
+    if check_error {
+        let expected_error =
+            fs::read_to_string(format!("{file_name}.e")).expect("Failed to read error file");
+        assert_eq!(error_output, expected_error);
+    }
+}
---
+++ tests/scoping_test.rs
@@ -0,0 +1,10 @@
+use common::run_test;
+use test_case::test_case;
+
+mod common;
+
+#[test_case("new_var_reads_outer_shadow", true, false; "New variable reads from outer shadow")]
+#[test_case("nested_writes_to_outer_vars", true, false; "Nested writes to outer variables")]
+fn scoping_tests(test_name: &str, check_output: bool, check_error: bool) {
+    run_test(test_name, check_output, check_error);
+}
