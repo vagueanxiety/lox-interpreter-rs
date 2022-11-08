73f3f7a78529e33b06867fec68b64925b2f6c3ad
Zhihang Zhang <hizhihang@gmail.com>
2022-11-07 21:15:19 -0800

    [test] add a simple test for closure

--- tests/ch10_test.rs
+++ tests/ch10_test.rs
@@ -3,6 +3,7 @@ use test_case::test_case;
 
 mod common;
 
+#[test_case("closure", true, false; "Closures")]
 #[test_case("simple_fun", true, false; "Simple function")]
 fn ch10_test(test_name: &str, check_output: bool, check_error: bool) {
     run_test(test_name, check_output, check_error);
