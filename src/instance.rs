f1d7007f993ac6664b921c0183f30dbb669545d3
Zhihang Zhang <hizhihang@gmail.com>
2022-11-21 14:28:24 -0800

    [refactor] use hash as key instead of nested map

--- src/instance.rs
+++ src/instance.rs
@@ -2,20 +2,23 @@ use crate::environment::EnvironmentTree;
 use crate::expr_interpret::{Result, RuntimeError};
 use crate::token::Token;
 use crate::{class::LoxClass, literal::Literal};
+use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 use std::{fmt::Display, rc::Rc};
 
 // Note that currently nothing stops you from putting an instance into
 // itself, even though it will create a reference cycle of Rc<Literal>.
 // We *might* be able to prevent it by doing some static analysis during variable resolution (but how do we handle a loop with
 // more than two Rc<Literal>?)
+
 #[derive(PartialEq)]
 pub struct LoxInstance {
     class: Rc<LoxClass>,
     fields: HashMap<String, Rc<Literal>>,
     bound_methods: HashMap<String, Rc<Literal>>,
-    // name of superclass -> name of method -> Rc<Literal::FunctionLiteral...>
-    bound_super_methods: HashMap<String, HashMap<String, Rc<Literal>>>,
+    // hash of (name of superclass, name of method) -> Rc<Literal::FunctionLiteral...>
+    bound_super_methods: HashMap<u64, Rc<Literal>>,
 }
 
 impl Display for LoxInstance {
@@ -72,22 +75,15 @@ impl LoxInstance {
         let method_name = &method.lexeme;
         let class_name = &superclass.name;
 
-        if let Some(map) = self.bound_super_methods.get_mut(class_name) {
-            if let Some(m) = map.get(method_name) {
-                // already bound
-                return Ok(m.clone());
-            } else if let Some(m) = superclass.find_method(method_name) {
-                // superclass entry exists but not the method
-                let bound_method = Rc::new(Literal::FunctionLiteral(m.bind(env, instance)));
-                map.insert(method_name.to_owned(), bound_method.clone());
-                return Ok(bound_method);
-            }
-        } else if let Some(m) = superclass.find_method(method_name) {
-            // both superclass and method don not exist
-            let bound_method = Rc::new(Literal::FunctionLiteral(m.bind(env, instance)));
-            let mut map = HashMap::new();
-            map.insert(method_name.to_owned(), bound_method.clone());
-            self.bound_super_methods.insert(class_name.clone(), map);
+        let mut hasher = DefaultHasher::new();
+        (class_name, method_name).hash(&mut hasher);
+        let hash = hasher.finish();
+
+        if let Some(bound_method) = self.bound_super_methods.get(&hash) {
+            return Ok(bound_method.clone());
+        } else if let Some(method) = superclass.find_method(method_name) {
+            let bound_method = Rc::new(Literal::FunctionLiteral(method.bind(env, instance)));
+            self.bound_super_methods.insert(hash, bound_method.clone());
             return Ok(bound_method);
         }
 
